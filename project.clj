1a0dba1da80f10508d210e158f0fca79a49e65df
Paul Lam <paul@quantisan.com>
2013-07-15 17:17:18 +0100

    lein profile for scala 2.8.0

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject clj-kafka/clj-kafka "0.1.0-0.8-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.uswitch/kafka_2.9.2 "0.8.0-SNAPSHOT"]
                  [zookeeper-clj "0.9.3"]
                  [org.clojure/data.json "0.2.2"]]
   :exclusions [javax.mail/mail
@@ -11,5 +10,7 @@
                jline/jline]
   :plugins [[lein-expectations "0.0.8"]]
   :profiles {:dev {:dependencies [[commons-io/commons-io "2.4"]
-                                  [expectations "1.4.45"]]}}
+                                  [expectations "1.4.45"]]}
+             :provided {:dependencies [[quantisan/kafka_2.9.2 "0.8.0-beta1-candidate1"]]}
+             :scala-2.8.0 {:dependencies [[quantisan/kafka_2.8.0 "0.8.0-beta1-candidate1"]]}}
   :description "Clojure wrapper for Kafka's Java API")
